0aecd856e3bc3f68f0346e382b50beb4cca9e0cd
graphicedit <contact@graphicedit.com>
2020-03-16 23:31:13 +0000

    Basic SideScroller
    Basic SideScroller Background and Little tidy up

---
+++ BlueProject/Assets/Scripts/BGScroller.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class BGScroller : MonoBehaviour
+{
+    public float scrollSpeed;
+    public float tileSizeX;
+
+    private Vector2 startPosition;
+
+    private void Start() {
+        startPosition = transform.position;
+    }
+
+    private void Update() {
+        var newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeX);
+        transform.position = startPosition + Vector2.left * newPosition;
+    }
+}
