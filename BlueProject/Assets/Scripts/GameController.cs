73e995e988532e8b0c15e8c2002b39aee85b7588
GraphicEdit <contact@graphicedit.com>
2020-03-16 20:06:04 +0000

    simple Control (#39)
    simple Control and temporary ship

---
+++ BlueProject/Assets/Scripts/GameController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+    public static GameController instance;
+
+    public bool isDead = false;
+
+
+    private void Awake() {
+        if(instance == null) {
+            instance = this;
+        }
+    }
+
+    public void PlayerCrash() {
+        this.isDead = true;
+
+    }
+
+    private void Update() {
+  
+        if (this.isDead && Input.GetButtonDown("Jump")) {
+            //...reload the current scene.
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+}
