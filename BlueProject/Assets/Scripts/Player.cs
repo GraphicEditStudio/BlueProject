86c4d27e667906272fa03d545ce6bc0393e5117d
AntonioMihaiDima <56171859+AntonioMihaiDima@users.noreply.github.com>
2020-03-18 15:31:03 +0000

    Coding repo (#42)
    * New scrolling background script and improved movement speed to enchance movement on both directions
    
    Background now scrolls onto a quad in both directions, initially the ship is in the middle, when it moves to a side it flips and the positioning on the x axis moves to the edge so it simulates acceleration. When there is no input anymore the ship returns back to the middle.
    
    * Shooting and heat bar UI mechanic implemented
    
    The ship shoots with a 0.5s cooldown and the heat bar goes down over time. When the bar is empty the player can't shoot anymore and needs to wait for the bar to fill but not necessarely fully.
    
    * Bug fixing
    
    There was a weird delay between the pressing button down and the way the shooting and heatbar mechanic occurs. Seems fixed for the most part

--- BlueProject/Assets/Scripts/Player.cs
+++ BlueProject/Assets/Scripts/Player.cs
@@ -8,11 +8,15 @@ public class Player : MonoBehaviour {
 
     bool moveup = false;
     bool movedown = false;
+    float lastshot = 0;
+    public static bool isShooting = false;
 
     public SpriteRenderer ShipRenderer;
 
-	// Use this for initialization
-	void Start ()
+    public Transform Projectile;
+
+    // Use this for initialization
+    void Start ()
     {
         ShipRenderer.GetComponent<SpriteRenderer>();
     }
@@ -20,43 +24,80 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        if (GameController.instance.isDead == false)
+
+        //if (GameController.instance.isDead == false)
         {
             if ((Input.GetAxisRaw("Vertical") > 0) && (transform.position.y <= 4.4f))
             {
-
+                
                 transform.Translate(Vector3.up * speed * Time.deltaTime);
             }
             else if ((Input.GetAxisRaw("Vertical") < 0) && (transform.position.y >= -4.4f))
             {
+                
                 transform.Translate(Vector3.down * speed * Time.deltaTime);
             }
              
             if ((Input.GetAxisRaw("Horizontal") > 0) && (transform.position.x >= -7))
             {
-                ShipRenderer.flipX = false;           
+                ShipRenderer.flipX = false;
+                ProjectileScript.flipX = false;
                 transform.Translate(Vector3.left * speed * 1.5f * Time.deltaTime);
             }
             else if ((Input.GetAxisRaw("Horizontal") < 0) && (transform.position.x <= 7))
             {
                 transform.Translate(Vector3.right * speed * 1.5f * Time.deltaTime);
                 ShipRenderer.flipX = true;
+                ProjectileScript.flipX = true;
             }
             else if (Input.GetAxisRaw("Horizontal") == 0)
             {
                 if (transform.position.x != 0)
-                    transform.position = Vector3.MoveTowards(transform.position, new Vector3(0, transform.position.y, transform.position.z), speed / 2 * 1.5f * Time.deltaTime);        
+                {                   
+                    transform.position = Vector3.MoveTowards(transform.position, new Vector3(0, transform.position.y, transform.position.z), speed * 1.5f * Time.deltaTime);
+                }
             }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                isShooting = true;
+            }
+
+            if ((Time.time - lastshot > 0.5f) && (isShooting))
+            {
+                if (!HeatBar.BarEmpty)
+                {
+                    isShooting = true;
+                    if (!ShipRenderer.flipX)
+                    {
+                        Instantiate(Projectile, new Vector3(transform.position.x + 1, transform.position.y, transform.position.z), Quaternion.identity);
+                    }
+                    else
+                    {
+                        Instantiate(Projectile, new Vector3(transform.position.x - 1, transform.position.y, transform.position.z), Quaternion.identity);
+                    }
+                }
+
+                lastshot = Time.time;
+            }
+
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                isShooting = false;
+            }
+            
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D coll) {
+    /*private void OnTriggerEnter2D(Collider2D coll) {
         //Debug.Log(coll.gameObject.tag);
         if (coll.gameObject.tag == "Enemy") {
             GameController.instance.PlayerCrash();
             this.gameObject.GetComponent<Animator>().CrossFade("Explosion",0);
             //this.gameObject.SetActive(false);
         }
+        
     }
+    */
     
 }
---
+++ BlueProject/Assets/Scripts/ProjectileScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileScript : MonoBehaviour
+{
+    public static bool flipX = false;
+    public static float speed = 30;
+    bool DirectionChosen = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!flipX)
+            DirectionChosen = false;
+        else
+            DirectionChosen = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+            if (!DirectionChosen)
+            {
+                transform.Translate(Vector3.right * speed * Time.deltaTime);
+            }
+            else
+            {
+                transform.Translate(Vector3.left * speed * Time.deltaTime);
+            }
+            if ((transform.position.x > 8) || (transform.position.x < -8))
+            {
+                Destroy(gameObject);
+            }
+        
+
+    }
+}
