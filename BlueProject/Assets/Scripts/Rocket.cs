7c62b624926a0302133e921de17c7c97e785fa11
Bug01000011Hunter <61810539+Bug01000011Hunter@users.noreply.github.com>
2020-03-20 00:15:44 +0700

    Player's rocket (#55)
    * Some code optimizations
    
    mainly changing the spawn method to coroutine
    
    * Update SpaceItems.cs
    
    * trying add force instead of translate
    
    * LaunchRocket
    
    * Movement tweaks
    
    Co-authored-by: GraphicEdit <contact@graphicedit.com>

---
+++ BlueProject/Assets/Scripts/Rocket.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D rb;
+    void Start()
+    {
+        Destroy(gameObject, 3f);
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(speed * Time.fixedDeltaTime * Vector2.right);
+    }
+}
---
+++ BlueProject/Assets/Scripts/ShootRocket.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootRocket : MonoBehaviour
+{
+    public Vector2 initForce;
+    public GameObject rocketPrefab;
+    KeyCode rocket = KeyCode.Space;
+    public int ammo;
+    void Update()
+    {
+        if (ammo > 0 && Input.GetKeyDown(rocket))
+        {
+            Instantiate(rocketPrefab, transform).GetComponent<Rigidbody2D>().AddForce(initForce, ForceMode2D.Impulse);
+            ammo--;
+        }
+    }
+}
