577a1a0bb554fedf0dbdd44f4b1d6c9c2df1d2e5
IAmBot102102 <53452898+IAmBot102102@users.noreply.github.com>
2020-03-21 15:04:39 -0400

    Sama testing (#69)
    * Added Test Game Over Screen
    
    * Fixed bug
    
    * Added temporary solution to the GAME OVER Text
    
    * Fixed errors with length and breaks in the Progressive Screen
    
    * Fixed errors with length and breaks in the Progressive Screen
    
    * Fixed errors with the interval in which the gameover screen progressively appears
    
    * Created Coroutine for the progressive game over
    
    * Fixed 1/0 issues
    
    Co-authored-by: IAmBot102102 <BotNumber102@gmail.com>

--- BlueProject/Assets/Scripts/GameController.cs
+++ BlueProject/Assets/Scripts/GameController.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,25 +8,37 @@ public class GameController : MonoBehaviour {
 
     public static GameController instance;
     public bool isDead = false;
+    public Text txt;
+    public float displayDelay = 36;
 
-
+    private IEnumerator Progressive(){
+        string temporaryText = "Game Over";
+        for(int i = 0; i < temporaryText.Length; i++){
+            txt.text += temporaryText[i];
+            yield return new WaitForSeconds(displayDelay);
+        }
+    }
     private void Awake() {
         if(instance == null) {
             instance = this;
         }
+        PlayerCrash();
     }
 
     public void PlayerCrash() {
         this.isDead = true;
-
+        // Display the Game Over Screen
+        StartCoroutine("Progressive");
     }
 
     private void Update() {
-  
         if (this.isDead && Input.GetButtonDown("Jump")) {
             //...reload the current scene.
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
+    
 
+
+    
 }
