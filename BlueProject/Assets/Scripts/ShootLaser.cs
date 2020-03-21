7a024bdd764894292b2ff32baffd5eccbd5ad589
Bug01000011Hunter <61810539+Bug01000011Hunter@users.noreply.github.com>
2020-03-21 09:41:15 +0700

    LaserShoot

---
+++ BlueProject/Assets/Scripts/ShootLaser.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootLaser : MonoBehaviour
+{
+    public GameObject laserPrefab;
+    KeyCode laser = KeyCode.Mouse0;
+    public int ammo;
+    public float cooldown;
+    float shootTime = 0;
+    void Update()
+    {
+        if (ammo > 0 && Input.GetKeyDown(laser))
+        {
+            if (Time.time > shootTime + cooldown)
+            {
+                shootTime = Time.time;
+                Instantiate(laserPrefab, transform);
+                ammo--;
+            }
+        }
+    }
+}
