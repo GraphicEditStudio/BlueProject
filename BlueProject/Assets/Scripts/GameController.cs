d471ace6c0b70892c0c2b96da6eaf6a3a6e9597b
IAmBot102102 <53452898+IAmBot102102@users.noreply.github.com>
2020-03-21 17:10:23 -0400

    Stage clear (#72)
    * Added Test Game Over Screen
    
    * Fixed bug
    
    * Added temporary solution to the GAME OVER Text
    
    * Fixed errors with length and breaks in the Progressive Screen
    
    * Fixed errors with length and breaks in the Progressive Screen
    
    * Fixed errors with the interval in which the gameover screen progressively appears
    
    * Created Coroutine for the progressive game over
    
    * Fixed 1/0 issues
    
    * Added default value to displayDelay and created the PlayerClear function
    
    * Activated the PlayerClear function
    
    * Deactivated the clear on load
    
    * Added comments so people understand
    
    Co-authored-by: IAmBot102102 <BotNumber102@gmail.com>

--- BlueProject/Assets/Scripts/GameController.cs
+++ BlueProject/Assets/Scripts/GameController.cs
@@ -3,14 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-
 public class GameController : MonoBehaviour {
-
     public static GameController instance;
     public bool isDead = false;
     public Text txt;
-    public float displayDelay = 36;
-
+    public float displayDelay = 0.45f;
     private IEnumerator Progressive(){
         string temporaryText = "Game Over";
         for(int i = 0; i < temporaryText.Length; i++){
@@ -18,27 +15,35 @@ public class GameController : MonoBehaviour {
             yield return new WaitForSeconds(displayDelay);
         }
     }
+
+    private IEnumerator ProgressiveClear(){
+        string temporaryText = "Cleared Level";
+        for(int i = 0; i < temporaryText.Length; i++){
+            txt.text += temporaryText[i];
+            yield return new WaitForSeconds(displayDelay);
+        }
+    }
+
     private void Awake() {
         if(instance == null) {
             instance = this;
         }
-        PlayerCrash();
+        
     }
-
     public void PlayerCrash() {
         this.isDead = true;
         // Display the Game Over Screen
         StartCoroutine("Progressive");
-    }
 
+    }
+    public void PlayerClear(){
+        // Displays the clear screen
+        StartCoroutine("ProgressiveClear");
+    }
     private void Update() {
         if (this.isDead && Input.GetButtonDown("Jump")) {
             //...reload the current scene.
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
-    
-
-
-    
 }
