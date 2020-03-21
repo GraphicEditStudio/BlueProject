318a740a850880538ae829a440971ec48555fa9b
IAmBot102102 <BotNumber102@gmail.com>
2020-03-21 15:36:24 -0400

    Added default value to displayDelay and created the PlayerClear function

--- BlueProject/Assets/Scripts/GameController.cs
+++ BlueProject/Assets/Scripts/GameController.cs
@@ -9,10 +9,17 @@ public class GameController : MonoBehaviour {
     public static GameController instance;
     public bool isDead = false;
     public Text txt;
-    public float displayDelay = 36;
+    public float displayDelay = 0.45f;
 
     private IEnumerator Progressive(){
         string temporaryText = "Game Over";
+        for(int i = 0; i < temporaryText.Length; i++){
+            txt.text += temporaryText[i];
+            yield return new WaitForSeconds(displayDelay);
+        }
+    }
+    private IEnumerator ProgressiveClear(){
+        string temporaryText = "Cleared Level";
         for(int i = 0; i < temporaryText.Length; i++){
             txt.text += temporaryText[i];
             yield return new WaitForSeconds(displayDelay);
@@ -22,13 +29,16 @@ public class GameController : MonoBehaviour {
         if(instance == null) {
             instance = this;
         }
-        PlayerCrash();
+        //PlayerCrash();
     }
 
     public void PlayerCrash() {
         this.isDead = true;
         // Display the Game Over Screen
         StartCoroutine("Progressive");
+    }
+    public void PlayerClear(){
+        StartCoroutine("ProgressiveClear");
     }
 
     private void Update() {
