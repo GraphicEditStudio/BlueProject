260c5205a754d4c2f7e917efb37b688f17e80e90
GraphicEdit <contact@graphicedit.com>
2020-03-28 23:10:09 +0000

    First DRAFT Level 1

---
+++ BlueProject/Assets/Scripts/LevelScroller.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class LevelScroller : MonoBehaviour
+{
+    public float scrollSpeed;
+    public float tileSizeX;
+
+    private Vector3 startPosition;
+	private float newPosition;
+
+    private void Start() {
+        startPosition = transform.position;
+    }
+
+    private void Update() {
+        newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeX);
+        transform.position = startPosition + Vector3.left * newPosition;
+    }
+}
