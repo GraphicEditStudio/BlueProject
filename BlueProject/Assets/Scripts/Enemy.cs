96b7bf39edea21a5a7aa14bf757123353b80f6ee
IAmBot102102 <BotNumber102@gmail.com>
2020-03-28 19:45:49 -0400

    Attempting to fix the enemies not dying.

--- BlueProject/Assets/Scripts/Enemy.cs
+++ BlueProject/Assets/Scripts/Enemy.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private Health healthObject;
     // Start is called before the first frame update
     void Start()
     {
-        
+        healthObject = GetComponent<Health>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(healthObject.healthPoints <= 0){
+            gameObject.setActive(false);
+        }
     }
 }
