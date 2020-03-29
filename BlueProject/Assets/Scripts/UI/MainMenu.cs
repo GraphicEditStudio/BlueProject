fb4b8ca7846654d7e28cbdcb0f679dfb278935fc
Bug01000011Hunter <61810539+Bug01000011Hunter@users.noreply.github.com>
2020-03-29 11:43:24 +0700

    ClickSound

--- BlueProject/Assets/Scripts/UI/MainMenu.cs
+++ BlueProject/Assets/Scripts/UI/MainMenu.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Audio;
 namespace Core{
     public class MainMenu : MonoBehaviour
     {
         public GameObject settingCanvas, mainCanvas;
+        AudioManager audioManager;
         private void Start()
         {
+            audioManager = AudioManager.instance;
             if (!settingCanvas)
             {
                 settingCanvas = GameObject.Find("SettingsCanvas");
@@ -23,32 +26,38 @@ namespace Core{
         }
         public void ToMainMenu()
         {
+            audioManager.Play("ButtonClick");
             SceneManager.LoadScene(0);
         }
         public void PlayGame()
         {
+            audioManager.Play("ButtonClick");
             SceneManager.LoadScene(1);
         }
 
         public void QuitGame()
         {
+            audioManager.Play("ButtonClick");
             Application.Quit();//only work when playing the .exe
         }
 
         public void BackButton()
         {
+            audioManager.Play("ButtonClick");
             settingCanvas.SetActive(false);
             mainCanvas.SetActive(true);
         }
 
         public void SettingsButton()
         {
+            audioManager.Play("ButtonClick");
             settingCanvas.SetActive(true);
             mainCanvas.SetActive(false);
         }
 
         public void RestartButton()
         {
+            audioManager.Play("ButtonClick");
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); //reload current scene
         }
     }
